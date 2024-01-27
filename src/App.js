e12653f38dc767bec37fb62d8d0571d58d9d3059
Luis Rangel <luisrangelc@gmail.com>
2024-01-27 12:26:15 -0600

    fix: first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './platzi.webp';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoCounter completed={5} total={10} />
+      <TodoSearch />
+      
+      <TodoCounter2 completed={15} total={30} />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
   );
+}
+
+function TodoSearch() {
+  return (<></>)
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+function TodoCounter(props) {
+  return (
+    <h1>
+      Completaste {props.completed} de {props.total} TODOs
+    </h1>
+  );
+}
+function TodoCounter2(props) {
+  return React.createElement('h1', null, `Completaste ${props.completed} de ${props.total} TODOs`);
 }
 
 export default App;
