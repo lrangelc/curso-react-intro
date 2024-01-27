0c0e3b6e6df18d8770332c26cb7ae4f1dbbc0127
Luis Rangel <luisrangelc@gmail.com>
2024-01-27 17:14:57 -0600

    feat: add state to TodoSearch component

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,7 +1,18 @@
 import './index.css';
 
 function CreateTodoButton() {
-  return <button className="CreateTodoButton">+</button>;
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={(event) => {
+        console.log('CreateTodoButton clicked...');
+        console.log(event);
+        console.log(event.target);
+      }}
+    >
+      +
+    </button>
+  );
 }
 
 export { CreateTodoButton };
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,7 +1,27 @@
+import React from 'react';
+
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log({ searchValue });
   return (
     <div className="TodoSearch">
-      <input placeholder="Cortar cebolla" />
+      <input
+        placeholder="Cortar cebolla"
+        value={searchValue}
+        onChange={(event) => {
+          // console.log('TodoSearch onChange...');
+          // console.clear();
+          // console.log(event.target.value);
+          setSearchValue((previousValue) => {
+            console.log({ previousValue });
+            return (previousValue = event.target.value);
+          });
+          // setSearchValue(() => {
+
+          // })
+        }}
+      />
     </div>
   );
 }
